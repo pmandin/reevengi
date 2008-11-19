57287c2d228545b558f5b0da9d28feae268624a5
Patrice Mandin <patmandin@gmail.com>
2008-11-19 21:37:38 +0000

    Add clipping routine

--- src/draw.c
+++ src/draw.c
@@ -22,13 +22,24 @@
 
 #include "video.h"
 
+/*--- Defines ---*/
+
+#define CLIP_LEFT_EDGE   0x1
+#define CLIP_RIGHT_EDGE  0x2
+#define CLIP_BOTTOM_EDGE 0x4
+#define CLIP_TOP_EDGE    0x8
+#define CLIP_INSIDE(a)   (!a)
+#define CLIP_REJECT(a,b) (a&b)
+#define CLIP_ACCEPT(a,b) (!(a|b))
+
 /*--- Variables ---*/
 
 static Uint32 draw_color = 0;
 
 /*--- Functions prototypes ---*/
 
-static void clip_line(int *x1, int *y1, int *x2, int *y2);
+static int clipEncode (int x, int y, int left, int top, int right, int bottom);
+static int clip_line(int *x1, int *y1, int *x2, int *y2);
 
 /*--- Functions ---*/
 
@@ -47,13 +58,14 @@ void draw_line(int x1, int y1, int x2, int y2)
 	int tmp, x=0, y=0, dx,dy, sx,sy, pixx,pixy;
 	Uint8 *pixel;
 
-	/* TODO: clip line to viewport */
-	clip_line(&x1, &y1, &x2, &y2);
-
 	x1 += video.viewport.x;
 	y1 += video.viewport.y;
 	x2 += video.viewport.x;
 	y2 += video.viewport.y;
+
+	if (!clip_line(&x1, &y1, &x2, &y2)) {
+		return;
+	}
 
 	dx = x2 - x1;
 	dy = y2 - y1;
@@ -72,7 +84,7 @@ void draw_line(int x1, int y1, int x2, int y2)
 		tmp = pixx; pixx = pixy; pixy = tmp;
 	}
 
-	printf("draw_line(%d,%d, %d,%d)\n", x1,y1, x2,y2);
+	/*printf("draw_line(%d,%d, %d,%d)\n", x1,y1, x2,y2);*/
 
 	switch(surf->format->BytesPerPixel) {
 		case 1:
@@ -112,6 +124,73 @@ void draw_line(int x1, int y1, int x2, int y2)
 }
 
 /* Clip line to viewport size */
-static void clip_line(int *x1, int *y1, int *x2, int *y2)
+static int clipEncode (int x, int y, int left, int top, int right, int bottom)
 {
+	int code = 0;
+
+	if (x < left) {
+		code |= CLIP_LEFT_EDGE;
+	} else if (x > right) {
+		code |= CLIP_RIGHT_EDGE;
+	}
+	if (y < top) {
+		code |= CLIP_TOP_EDGE;
+	} else if (y > bottom) {
+		code |= CLIP_BOTTOM_EDGE;
+	}
+
+	return code;
+}
+
+static int clip_line(int *x1, int *y1, int *x2, int *y2)
+{
+	int left = video.viewport.x;
+	int top = video.viewport.y;
+	int right = left+video.viewport.w;
+	int bottom = top+video.viewport.h;
+
+	int draw = 0;
+	for(;;) {
+		int code1 = clipEncode(*x1, *y1, left, top, right, bottom);
+		int code2 = clipEncode(*x2, *y2, left, top, right, bottom);
+		float m;
+
+		if (CLIP_ACCEPT(code1, code2)) {
+			return 1;
+		}
+
+		if (CLIP_REJECT(code1, code2)) {
+			return 0;
+		}
+		
+		if (CLIP_INSIDE(code1)) {
+			int swaptmp = *x2; *x2 = *x1; *x1 = swaptmp;
+			swaptmp = *y2; *y2 = *y1; *y1 = swaptmp;
+			swaptmp = code2; code2 = code1; code1 = swaptmp;
+		}
+
+		m = 1.0f;
+		if (*x2 != *x1) {
+			m = (*y2 - *y1) / (float)(*x2 - *x1);
+		}
+		if (code1 & CLIP_LEFT_EDGE) {
+			*y1 += (int)((left - *x1) * m);
+			*x1 = left;
+		} else if (code1 & CLIP_RIGHT_EDGE) {
+			*y1 += (int)((right - *x1) * m);
+			*x1 = right;
+		} else if (code1 & CLIP_BOTTOM_EDGE) {
+			if (*x2 != *x1) {
+				*x1 += (int)((bottom - *y1) / m);
+			}
+			*y1 = bottom;
+		} else if (code1 & CLIP_TOP_EDGE) {
+			if (*x2 != *x1) {
+				*x1 += (int)((top - *y1) / m);
+			}
+			*y1 = top;
+		}
+	}
+
+	return draw;
 }
