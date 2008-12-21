411fde965fb0290c62d0a3ccbe84018161d4b4f4
Patrice Mandin <patmandin@gmail.com>
2008-12-21 19:10:03 +0000

    Read source r,g,b

--- src/dither.c
+++ src/dither.c
@@ -1,5 +1,5 @@
 /*
-	Dithering
+	Floyd-Steinberg dithering
 
 	Copyright (C) 2008	Patrice Mandin
 
@@ -107,6 +107,7 @@ void dither(SDL_Surface *src, SDL_Surface *dest)
 static void dither2(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
 {
 	int x,y;
+	Uint8 r,g,b;
 	Uint16 *src_line;
 	Uint8 *dst_line;
 	Uint8 *err_cur = &errbuffer[4];
@@ -118,6 +119,7 @@ static void dither2(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
 		Uint16 *src_col = src_line;
 		Uint8 *dst_col = dst_line;
 		for (x=0; x<dest->w; x++) {
+			SDL_GetRGB(*src_col++, src->format, &r, &g, &b);
 		}
 		src_line += src->pitch>>1;
 		dst_line += dest->pitch;
@@ -127,6 +129,7 @@ static void dither2(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
 static void dither3(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
 {
 	int x,y;
+	Uint8 r,g,b;
 	Uint8 *src_line;
 	Uint8 *dst_line;
 	Uint8 *err_cur = &errbuffer[4];
@@ -137,6 +140,8 @@ static void dither3(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
 		Uint8 *src_col = src_line;
 		Uint8 *dst_col = dst_line;
 		for (x=0; x<dest->w; x++) {
+			SDL_GetRGB(*src_col, src->format, &r, &g, &b);
+			src_col += 3;
 		}
 		src_line += src->pitch;
 		dst_line += dest->pitch;
@@ -146,8 +151,9 @@ static void dither3(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
 static void dither4(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
 {
 	int x,y;
-	Uint8 *dst_line;
+	Uint8 r,g,b;
 	Uint32 *src_line;
+	Uint8 *dst_line;
 	Uint8 *err_cur = &errbuffer[4];
 	Uint8 *err_next = &errbuffer[4+dest->w+2];
 
@@ -157,6 +163,7 @@ static void dither4(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
 		Uint32 *src_col = src_line;
 		Uint8 *dst_col = dst_line;
 		for (x=0; x<dest->w; x++) {
+			SDL_GetRGB(*src_col++, src->format, &r, &g, &b);
 		}
 		src_line += src->pitch>>2;
 		dst_line += dest->pitch;
