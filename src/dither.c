27b4df61816a71fa1c8926ee9261f3f7d8ffcdd7
Patrice Mandin <patmandin@gmail.com>
2008-12-23 16:54:28 +0000

    Dither works now

--- src/dither.c
+++ src/dither.c
@@ -20,6 +20,16 @@
 
 #include <SDL.h>
 
+/*--- Defines ---*/
+
+#define FIND_COLOR_INDEX(r,g,b) \
+	(approxR1[r]+approxG1[g]+approxB1[b])
+
+#define FIND_APPROX(i, r,g,b) \
+	r = approxR2[i]; \
+	g = approxG2[i]; \
+	b = approxB2[i];
+
 /*--- Constants ---*/
 
 /* Map insensity for 216 color palette */
@@ -32,16 +42,12 @@ static const int map_color[6] = {
 static int inited = 0;
 static Uint8 approxR1[256], approxG1[256], approxB1[256];
 static Uint8 approxR2[216], approxG2[216], approxB2[216];
-static Uint16 fact1[512], fact2[512], fact3[512], fact4[512];
+static Sint16 fact1[512], fact2[512], fact3[512], fact4[512];
 static Uint8 sat[768];
 
 /*--- Functions prototypes ---*/
 
 static void dither_init(void);
-
-static void dither2(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer);
-static void dither3(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer);
-static void dither4(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer);
 
 /*--- Functions ---*/
 
@@ -68,11 +74,17 @@ void dither_setpalette(SDL_Surface *src)
 void dither(SDL_Surface *src, SDL_Surface *dest)
 {
 	void *errbuffer;
+	int x,y,line;
+	Uint8 r,g,b, r1,g1,b1, r2,g2,b2;
+	Sint16 dr,dg,db;
+	Uint8 *dst_line;
+	Sint16 *err_line[2];
+	Uint8 smix[3], idx;
 
 	if (!src || !dest) {
 		return;
 	}
-	if ((src->format->BytesPerPixel==1) || (dest->format->BytesPerPixel!=1)) {
+	if ((src->format->BytesPerPixel==1) || (dest->format->BytesPerPixel!=1) || (src->h<2)) {
 		return;
 	}
 
@@ -81,21 +93,132 @@ void dither(SDL_Surface *src, SDL_Surface *dest)
 		inited = 1;
 	}
 
-	errbuffer = calloc(2, (dest->w+2) * sizeof(Uint32));
+	errbuffer = calloc(2, (dest->w+2) * sizeof(Uint32)*2);
 	if (!errbuffer) {
 		fprintf(stderr, "Can not allocate memory for error buffer\n");
 		return;
 	}
 
+	err_line[0] = &((Sint16 *) errbuffer)[4+(dest->w+2)*0];
+	err_line[1] = &((Sint16 *) errbuffer)[4+(dest->w+2)*1];
+	line = 0;
+
 	switch(src->format->BytesPerPixel) {
 		case 2:
-			dither2(src, dest, errbuffer);
-			break;
-		case 3:
-			dither3(src, dest, errbuffer);
+			{
+				Uint16 *src_line, *src_col;
+				Uint8 *dst_col;
+				Sint16 *err_col, *err_next;
+
+				src_line = (Uint16 *) src->pixels;
+				dst_line = dest->pixels;
+				for (y=0; y<dest->h; y++) {
+					src_col = src_line;
+					dst_col = dst_line;
+					err_col = err_line[line];
+					err_next = err_line[line ^ 1];
+					err_next[0] = err_next[1] = err_next[2] = 0;
+					for (x=0; x<dest->w; x++) {
+						SDL_GetRGB(*src_col++, src->format, &r, &g, &b);
+
+						r1 = sat[256 + r + err_col[0]];
+						g1 = sat[256 + g + err_col[1]];
+						b1 = sat[256 + b + err_col[2]];
+
+						idx = FIND_COLOR_INDEX(r1,g1,b1);
+
+						FIND_APPROX(idx, r2,g2,b2);
+
+						dr = r1-r2;
+						dg = g1-g2;
+						db = b1-b2;
+
+						*dst_col++ = 16+idx;
+
+						err_col[4+0] += fact1[255+dr];
+						err_col[4+1] += fact1[255+dg];
+						err_col[4+2] += fact1[255+db];
+
+						err_next[-4+0] += fact2[255+dr];
+						err_next[-4+1] += fact2[255+dg];
+						err_next[-4+2] += fact2[255+db];
+
+						err_next[0] += fact3[255+dr];
+						err_next[1] += fact3[255+dg];
+						err_next[2] += fact3[255+db];
+
+						err_next[4+0] = fact4[255+dr];
+						err_next[4+1] = fact4[255+dg];
+						err_next[4+2] = fact4[255+db];
+
+						err_col += 4;
+						err_next += 4;
+					}
+
+					/* Next line */
+					src_line += src->pitch>>1;
+					dst_line += dest->pitch;
+					line ^= 1;
+				}
+			}
 			break;
 		case 4:
-			dither4(src, dest, errbuffer);
+			{
+				Uint32 *src_line, *src_col;
+				Uint8 *dst_col;
+				Sint16 *err_col, *err_next;
+
+				src_line = (Uint32 *) src->pixels;
+				dst_line = dest->pixels;
+				for (y=0; y<dest->h; y++) {
+					src_col = src_line;
+					dst_col = dst_line;
+					err_col = err_line[line];
+					err_next = err_line[line ^ 1];
+					err_next[0] = err_next[1] = err_next[2] = 0;
+					for (x=0; x<dest->w; x++) {
+						SDL_GetRGB(*src_col++, src->format, &r, &g, &b);
+
+						r1 = sat[255 + r + err_col[0]];
+						g1 = sat[255 + g + err_col[1]];
+						b1 = sat[255 + b + err_col[2]];
+
+						idx = FIND_COLOR_INDEX(r1,g1,b1);
+
+						FIND_APPROX(idx, r2,g2,b2);
+
+						dr = r1-r2;
+						dg = g1-g2;
+						db = b1-b2;
+
+						*dst_col++ = 16+idx;
+
+						err_col[4+0] += fact1[255+dr];
+						err_col[4+1] += fact1[255+dg];
+						err_col[4+2] += fact1[255+db];
+
+						err_next[-4+0] += fact2[255+dr];
+						err_next[-4+1] += fact2[255+dg];
+						err_next[-4+2] += fact2[255+db];
+
+						err_next[0] += fact3[255+dr];
+						err_next[1] += fact3[255+dg];
+						err_next[2] += fact3[255+db];
+
+						err_next[4+0] = fact4[255+dr];
+						err_next[4+1] = fact4[255+dg];
+						err_next[4+2] = fact4[255+db];
+
+						err_col += 4;
+						err_next += 4;
+					}
+
+					/* Next line */
+					src_line += src->pitch>>2;
+					dst_line += dest->pitch;
+					line ^= 1;
+				}
+			}
 			break;
 		default:
 			break;
@@ -104,81 +227,15 @@ void dither(SDL_Surface *src, SDL_Surface *dest)
 	free(errbuffer);
 }
 
-static void dither2(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
-{
-	int x,y;
-	Uint8 r,g,b;
-	Uint16 *src_line;
-	Uint8 *dst_line;
-	Uint8 *err_cur = &errbuffer[4];
-	Uint8 *err_next = &errbuffer[4+dest->w+2];
-
-	src_line = (Uint16 *) src->pixels;
-	dst_line = dest->pixels;
-	for (y=0; y<dest->h; y++) {
-		Uint16 *src_col = src_line;
-		Uint8 *dst_col = dst_line;
-		for (x=0; x<dest->w; x++) {
-			SDL_GetRGB(*src_col++, src->format, &r, &g, &b);
-		}
-		src_line += src->pitch>>1;
-		dst_line += dest->pitch;
-	}
-}
-
-static void dither3(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
-{
-	int x,y;
-	Uint8 r,g,b;
-	Uint8 *src_line;
-	Uint8 *dst_line;
-	Uint8 *err_cur = &errbuffer[4];
-	Uint8 *err_next = &errbuffer[4+dest->w+2];
-
-	dst_line = dest->pixels;
-	for (y=0; y<dest->h; y++) {
-		Uint8 *src_col = src_line;
-		Uint8 *dst_col = dst_line;
-		for (x=0; x<dest->w; x++) {
-			SDL_GetRGB(*src_col, src->format, &r, &g, &b);
-			src_col += 3;
-		}
-		src_line += src->pitch;
-		dst_line += dest->pitch;
-	}
-}
-
-static void dither4(SDL_Surface *src, SDL_Surface *dest, Uint8 *errbuffer)
-{
-	int x,y;
-	Uint8 r,g,b;
-	Uint32 *src_line;
-	Uint8 *dst_line;
-	Uint8 *err_cur = &errbuffer[4];
-	Uint8 *err_next = &errbuffer[4+dest->w+2];
-
-	src_line = (Uint32 *) src->pixels;
-	dst_line = dest->pixels;
-	for (y=0; y<dest->h; y++) {
-		Uint32 *src_col = src_line;
-		Uint8 *dst_col = dst_line;
-		for (x=0; x<dest->w; x++) {
-			SDL_GetRGB(*src_col++, src->format, &r, &g, &b);
-		}
-		src_line += src->pitch>>2;
-		dst_line += dest->pitch;
-	}
-}
-
 static void dither_init(void)
 {
 	int r,g,b,i,j;
 
 	for (i=0; i<256; i++) {
 		int j = (i*6)/256;
-		approxR1[i] = j;
+		approxR1[i] = j*36;
 		approxG1[i] = j*6;
-		approxB1[i] = j*36;
+		approxB1[i] = j;
 	}
 
 	for (r=0; r<6; r++) {
