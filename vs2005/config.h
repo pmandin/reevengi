63707ba9d1620c8d4f1ee845fb279c576381a210
Patrice Mandin <patmandin@gmail.com>
2008-12-26 17:38:33 +0000

    Use pregenerated config.h file for vs2005

---
+++ vs2005/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H 1
+
+#define PACKAGE_NAME "reevengi"
+
+#define PACKAGE_STRING PACKAGE_NAME "0.9"
+
+#define ENABLE_OPENGL 1
+
+/*#define ENABLE_MOVIES 1*/
+
+#endif /* CONFIG_H */
