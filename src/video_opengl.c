efdddd76fbb454f5ab4d91afc589cded340ae4ff
Patrice Mandin <patmandin@gmail.com>
2009-02-01 13:00:03 +0000

    Can always query extensions from string

--- src/video_opengl.c
+++ src/video_opengl.c
@@ -134,32 +134,16 @@ static void setVideoMode(video_t *this, int width, int height, int bpp)
 	this->has_gl_arb_texture_non_power_of_two = (strstr(extensions, "GL_ARB_texture_non_power_of_two") != NULL);
 	logMsg(2, "GL_ARB_texture_non_power_of_two: %d\n", this->has_gl_arb_texture_non_power_of_two);
 
-#if defined(GL_ARB_texture_rectangle)
 	this->has_gl_arb_texture_rectangle = (strstr(extensions, "GL_ARB_texture_rectangle") != NULL);
-#else
-	this->has_gl_arb_texture_rectangle = 0;
-#endif
 	logMsg(2, "GL_ARB_texture_rectangle: %d\n", this->has_gl_arb_texture_rectangle);
 
-#if defined(GL_EXT_paletted_texture)
 	this->has_gl_ext_paletted_texture = (strstr(extensions, "GL_EXT_paletted_texture") != NULL);
-#else
-	this->has_gl_ext_paletted_texture = 0;
-#endif
 	logMsg(2, "GL_EXT_paletted_texture: %d\n", this->has_gl_ext_paletted_texture);
 
-#if defined(GL_EXT_texture_rectangle)
 	this->has_gl_ext_texture_rectangle = (strstr(extensions, "GL_EXT_texture_rectangle") != NULL);
-#else
-	this->has_gl_ext_texture_rectangle = 0;
-#endif
 	logMsg(2, "GL_EXT_texture_rectangle: %d\n", this->has_gl_ext_texture_rectangle);
 
-#if defined(GL_NV_texture_rectangle)
 	this->has_gl_nv_texture_rectangle = (strstr(extensions, "GL_NV_texture_rectangle") != NULL);
-#else
-	this->has_gl_nv_texture_rectangle = 0;
-#endif
 	logMsg(2, "GL_NV_texture_rectangle: %d\n", this->has_gl_nv_texture_rectangle);
 
 	video.initViewport(&video);
