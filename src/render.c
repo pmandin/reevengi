09f449ed0d1cee90099d2776126fe5e26a2ce9cb
Patrice Mandin <patmandin@gmail.com>
2009-02-02 19:45:52 +0000

    Simplify

--- src/render.c
+++ src/render.c
@@ -352,27 +352,16 @@ static void sortBackToFront(int num_vtx, int *num_idx, vertex_t *vtx)
 
 	/* Then sort them */
 	for (i=0; i<num_vtx-1; i++) {
-		float dist_i = /* vtx1[i].pos[0]*vtx1[i].pos[0]
-			+ vtx1[i].pos[1]*vtx1[i].pos[1]
-			+ vtx1[i].pos[2]* */ vtx1[i].pos[2];
 
 		/* Find the farer, swap against the one at pos i */
 		for (j=i+1; j<num_vtx; j++) {
-			float dist_j = /*vtx1[j].pos[0]*vtx1[j].pos[0]
-				+ vtx1[j].pos[1]*vtx1[j].pos[1]
-				+ vtx1[j].pos[2]* */ vtx1[j].pos[2];
 
-			if (dist_i<dist_j) {
-				float tmp_dist;
+			if (vtx1[i].pos[2]<vtx1[j].pos[2]) {
 				vertex_t tmp_vertex;
 				vertexf_t tmp_vertexf;
 				int tmp_idx;
 			
 				/* Swap vtx at i,j */
-				tmp_dist = dist_i;
-				dist_i = dist_j;
-				dist_j = tmp_dist;
-
 				memcpy(&tmp_vertex, &vtx[i], sizeof(vertex_t));
 				memcpy(&vtx[i], &vtx[j], sizeof(vertex_t));
 				memcpy(&vtx[j], &tmp_vertex, sizeof(vertex_t));
