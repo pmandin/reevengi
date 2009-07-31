d579e58a909480a64be9289379971f79d12b67fa
Patrice Mandin <patmandin@gmail.com>
2009-07-31 16:05:13 +0000

    Change log level for fps

--- src/video.c
+++ src/video.c
@@ -244,7 +244,7 @@ static void countFps(video_t *this)
 
 	++this->fps;
 	if (cur_tick-this->start_tick>1000) {
-		logMsg(2, "video: %d fps\n", this->fps);
+		logMsg(5, "video: %d fps\n", this->fps);
 		this->fps = 0;
 		this->start_tick = cur_tick;
 	}
