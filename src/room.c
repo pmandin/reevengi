fb8a937a81b6454fd5cd35da44b411b63af23cb6
Patrice Mandin <patmandin@gmail.com>
2009-08-05 16:55:43 +0000

    Display offset

--- src/room.c
+++ src/room.c
@@ -503,12 +503,11 @@ static void scriptDump(room_t *this)
 			if (inst_len==0) {
 				inst_len = 16;
 			}
+			logMsg(4, "0x%08x: ", this->cur_inst_offset);
 			for (i=0; i<inst_len; i++) {
-				logMsg(4, "%02x ", this->cur_inst[i]);
+				logMsg(4, " %02x", this->cur_inst[i]);
 			}
-			if (inst_len>0) {
-				logMsg(4, "\n");
-			}
+			logMsg(4, "\n");
 		}
 
 		this->scriptPrivPrintInst(this);
