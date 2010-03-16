73e7a5fe9d8fb6ff1b9840f8843c87a6ba81d27a
Patrice Mandin <patmandin@gmail.com>
2010-03-16 12:54:48 +0000

    Display stage,room,camera in menu

--- src/menu.c
+++ src/menu.c
@@ -22,6 +22,11 @@
 #include "config.h"
 #endif
 
+#include <SDL.h>
+
+#include "render_texture.h"
+#include "render_skel.h"
+#include "state.h"
 #include "render_text.h"
 
 /*--- Defines ---*/
@@ -29,13 +34,72 @@
 #define START_X	16
 #define START_Y 16
 
+#define MENU_MAIN 0
+
+enum {
+	STR_TYPE_TEXT=0,
+	STR_TYPE_INT,
+	STR_TYPE_HEXA2,
+	STR_TYPE_HEXA4,
+	STR_TYPE_HEXA8
+};
+
+/*--- Types ---*/
+
+typedef struct {
+	int x,y;	/* Top left position (in chars) */
+	int strtype;
+	void *str;	/* Menu item, or ptr to int value */
+} menu_t;
+
 /*--- Constants ---*/
 
-static char *title=PACKAGE_STRING;
+menu_t main_menu[7]={
+	{0,0, STR_TYPE_TEXT, PACKAGE_STRING},
+	{0,2, STR_TYPE_TEXT, "Stage :"},
+	{8,2, STR_TYPE_INT, &game_state.num_stage},
+	{0,3, STR_TYPE_TEXT, "Room  :"},
+	{8,3, STR_TYPE_HEXA2, &game_state.num_room},
+	{0,4, STR_TYPE_TEXT, "Camera:"},
+	{8,4, STR_TYPE_HEXA2, &game_state.num_camera}
+};
 
 /*--- Functions ---*/
 
 void menu_render(void)
 {
-	render_text(title,START_X,START_Y);
+	int i, x,y;
+	char tmpstr[16];
+
+	x = START_X;
+	y = START_Y;
+	for (i=0; i<sizeof(main_menu)/sizeof(menu_t); i++) {
+		int px = x + (main_menu[i].x*8);
+		int py = y + (main_menu[i].y*11);
+		char *str = (char *) main_menu[i].str;
+		int *ptr_int = (int *) main_menu[i].str;
+
+		switch(main_menu[i].strtype) {
+			case STR_TYPE_TEXT:
+				break;
+			case STR_TYPE_INT:
+				sprintf(tmpstr,"%d", *ptr_int);
+				str = tmpstr;
+				break;
+			case STR_TYPE_HEXA2:
+				sprintf(tmpstr,"%02x", *ptr_int);
+				str = tmpstr;
+				break;
+			case STR_TYPE_HEXA4:
+				sprintf(tmpstr,"%04x", *ptr_int);
+				str = tmpstr;
+				break;
+			case STR_TYPE_HEXA8:
+				sprintf(tmpstr,"%08x", *ptr_int);
+				str = tmpstr;
+				break;
+		}
+
+		render_text(str, px, py);
+	}
 }
