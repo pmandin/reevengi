b4e96acd7e9afff97d16e2eef39d803068f1b150
Patrice Mandin <patmandin@gmail.com>
2010-05-12 06:29:04 +0000

    Use render_mask

--- src/room_rdt2.c
+++ src/room_rdt2.c
@@ -139,6 +139,7 @@ static void rdt2_getBoundary(room_t *this, int num_boundary, room_camswitch_t *r
 
 static void rdt2_displayTexts(room_t *this, int num_lang);
 
+static void rdt2_initMasks(room_t *this, int num_camera);
 static void rdt2_drawMasks(room_t *this, int num_camera);
 
 /*--- Functions ---*/
@@ -454,8 +455,108 @@ void room_rdt2_getText(room_t *this, int lang, int num_text, char *buffer, int b
 	}
 }
 
+static void rdt2_initMasks(room_t *this, int num_camera)
+{
+	rdt2_header_t *rdt_header = (rdt2_header_t *) this->file;
+	Uint32 offset;
+	rdt_camera_pos_t *cam_array;
+	rdt_mask_header_t *mask_hdr;
+	rdt_mask_offset_t *mask_offsets;
+	int num_offset;
+	render_mask_t *rdr_mask;
+
+	if (num_camera>=this->num_cameras) {
+		return;
+	}
+
+	if (game_state.bg_mask==NULL) {
+		return;
+	}
+
+	offset = SDL_SwapLE32(rdt_header->offsets[RDT2_OFFSET_CAMERAS]);
+	cam_array = (rdt_camera_pos_t *) &((Uint8 *) this->file)[offset];
+
+	offset = SDL_SwapLE32(cam_array[num_camera].masks_offset);
+	if (offset == 0xffffffffUL) {
+		return;
+	}
+
+	game_state.rdr_mask = render.render_mask_create(game_state.bg_mask);
+	if (!game_state.rdr_mask) {
+		return;
+	}
+	rdr_mask = game_state.rdr_mask;
+
+	mask_hdr = (rdt_mask_header_t *) &((Uint8 *) this->file)[offset];
+	offset += sizeof(rdt_mask_header_t);
+
+	mask_offsets = (rdt_mask_offset_t *) &((Uint8 *) this->file)[offset];
+	offset += sizeof(rdt_mask_offset_t) * SDL_SwapLE16(mask_hdr->num_offset);
+
+	for (num_offset=0; num_offset<SDL_SwapLE16(mask_hdr->num_offset); num_offset++) {
+		int num_mask;
+		
+		for (num_mask=0; num_mask<SDL_SwapLE16(mask_offsets->count); num_mask++) {
+			rdt_mask_square_t *square_mask;
+			int src_x, src_y, width, height, depth;
+			int dst_x = SDL_SwapLE16(mask_offsets->dst_x);
+			int dst_y = SDL_SwapLE16(mask_offsets->dst_y);
+			int scaled_dst_x, scaled_dst_y;
+			int scaled_dst_w, scaled_dst_h;
+
+			square_mask = (rdt_mask_square_t *) &((Uint8 *) this->file)[offset];
+			if (square_mask->size == 0) {
+				/* Rect mask */
+				rdt_mask_rect_t *rect_mask = (rdt_mask_rect_t *) square_mask;
+
+				src_x = rect_mask->src_x;
+				src_y = rect_mask->src_y;
+				dst_x += rect_mask->dst_x;
+				dst_y += rect_mask->dst_y;
+				width = SDL_SwapLE16(rect_mask->width);
+				height = SDL_SwapLE16(rect_mask->height);
+				depth = SDL_SwapLE16(rect_mask->depth);
+
+				offset += sizeof(rdt_mask_rect_t);
+			} else {
+				/* Square mask */
+
+				src_x = square_mask->src_x;
+				src_y = square_mask->src_y;
+				dst_x += square_mask->dst_x;
+				dst_y += square_mask->dst_y;
+				width = height = SDL_SwapLE16(square_mask->size);
+				depth = SDL_SwapLE16(square_mask->depth);
+
+				offset += sizeof(rdt_mask_square_t);
+			}
+
+			rdr_mask->addZone(rdr_mask,
+				src_x,src_y, width,height,
+				dst_x,dst_y, 32*depth);
+		}
+
+		mask_offsets++;
+	}
+
+	rdr_mask->finishedZones(rdr_mask);
+}
+
 static void rdt2_drawMasks(room_t *this, int num_camera)
 {
+#if 1
+	render_mask_t *rdr_mask;
+
+	if (!game_state.rdr_mask) {
+		rdt2_initMasks(this, num_camera);
+	}
+	rdr_mask = game_state.rdr_mask;
+	if (!rdr_mask) {
+		return;
+	}
+
+	rdr_mask->drawMask(rdr_mask);
+#else
 	rdt2_header_t *rdt_header = (rdt2_header_t *) this->file;
 	Uint32 offset;
 	rdt_camera_pos_t *cam_array;
@@ -555,4 +656,5 @@ static void rdt2_drawMasks(room_t *this, int num_camera)
 	render.bitmap.setMasking(0);
 	render.set_blending(0);
 	render.set_dithering(0);
+#endif
 }
