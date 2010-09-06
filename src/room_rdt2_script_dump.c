c950e31448e74219034e8b36c17840e9c115ac7f
Patrice Mandin <patmandin@gmail.com>
2010-09-06 06:31:36 +0000

    Max number of items

--- src/room_rdt2_script_dump.c
+++ src/room_rdt2_script_dump.c
@@ -677,7 +677,7 @@ static void scriptDumpBlock(room_t *this, script_inst_t *inst, Uint32 offset, in
 					strcat(strBuf, tmpBuf);
 					logMsg(1, "0x%08x: %s", offset, strBuf);
 
-					if (inst->item_set.type < 64) {
+					if (inst->item_set.type < 0x64) {
 						sprintf(strBuf, "#\t%s\n", item_name[inst->item_set.type]);
 					} else {
 						sprintf(strBuf, "#\tUnknown item\n");
@@ -737,7 +737,7 @@ static void scriptDumpBlock(room_t *this, script_inst_t *inst, Uint32 offset, in
 					strcat(strBuf, tmpBuf);
 					logMsg(1, "0x%08x: %s", offset, strBuf);
 
-					if (inst->item_add.id < 64) {
+					if (inst->item_add.id < 0x64) {
 						sprintf(strBuf, "#\t%s\n", item_name[inst->item_add.id]);
 					} else {
 						sprintf(strBuf, "#\tUnknown item\n");
