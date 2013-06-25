a51a670455f967dc232510d934a9020c45c43a8d
Patrice Mandin <patmandin@gmail.com>
2013-06-25 18:52:04 +0000

    null check

--- src/g_re2/rdt_scd.c
+++ src/g_re2/rdt_scd.c
@@ -224,6 +224,9 @@ Uint8 *rdt2_scd_scriptInit(room_t *this, int num_script)
 	}
 
 	rdt_header = (rdt2_header_t *) this->file;
+	if (!rdt_header) {
+		return NULL;
+	}
 	offset = SDL_SwapLE32(rdt_header->offsets[room_script]);
 
 	this->script_length = this->cur_inst_offset = 0;
