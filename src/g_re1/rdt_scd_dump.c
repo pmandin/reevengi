ac35418a229b5f28add69ff1ee953fb0c8124227
Patrice Mandin <patmandin@gmail.com>
2013-07-24 21:22:22 +0000

    re1 has a second script

--- src/g_re1/rdt_scd_dump.c
+++ src/g_re1/rdt_scd_dump.c
@@ -61,6 +61,8 @@ static char tmpBuf[256];
 
 /*--- Functions prototypes ---*/
 
+static void scd_scriptDump(room_t *this, int room_script);
+
 static void reindent(int num_indent);
 static void scriptDumpBlock(room_t *this, script_inst_t *inst, Uint32 offset, int length, int indent);
 
@@ -68,16 +70,28 @@ static void scriptDumpBlock(room_t *this, script_inst_t *inst, Uint32 offset, in
 
 void rdt1_scd_scriptDump(room_t *this, int num_script)
 {
-	rdt1_header_t *rdt_header;
-	Uint32 offset, script_length;
-	int room_script = RDT1_OFFSET_INIT_SCRIPT;
-	Uint8 *scriptPtr;
-
 	assert(this);
 
-	if (num_script == ROOM_SCRIPT_RUN) {
-		room_script = RDT1_OFFSET_ROOM_SCRIPT;
+	logMsg(1, "rdt: Dumping script %d\n", num_script);
+	switch(num_script) {
+		case ROOM_SCRIPT_INIT:
+			scd_scriptDump(this, RDT1_OFFSET_INIT_SCRIPT);
+			break;
+		case ROOM_SCRIPT_RUN:
+			scd_scriptDump(this, RDT1_OFFSET_ROOM_SCRIPT);
+			logMsg(1, "rdt: Dumping second script\n");
+			scd_scriptDump(this, RDT1_OFFSET_ROOM_SCRIPT2);
+			break;
+		default:
+			break;
 	}
+}
+
+static void scd_scriptDump(room_t *this, int room_script)
+{
+	rdt1_header_t *rdt_header;
+	Uint32 offset, script_length;
+	Uint8 *scriptPtr;
 
 	rdt_header = (rdt1_header_t *) this->file;
 	offset = SDL_SwapLE32(rdt_header->offsets[room_script]);
@@ -89,7 +103,6 @@ void rdt1_scd_scriptDump(room_t *this, int num_script)
 	scriptPtr = & (((Uint8 *) this->file)[offset]);
 	script_length = SDL_SwapLE16(*((Uint16 *) scriptPtr));
 
-	logMsg(1, "rdt: Dumping script %d\n", num_script);
 	logMsg(1, "0x%08x: BEGIN_FUNC func00\n", offset+2);
 	scriptDumpBlock(this, (script_inst_t *) &scriptPtr[2], offset+2, script_length-2, 1);
 	logMsg(1, "          : END_FUNC\n\n");
