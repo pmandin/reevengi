dfaa3b74d8b70edb941e2198275d0e5762389426
Patrice Mandin <patmandin@gmail.com>
2013-08-17 13:33:20 +0000

    Add header for events

---
+++ src/g_re1/rdt_evt.h
@@ -0,0 +1,71 @@
+/*
+	RE1 EVT
+	Room events?
+
+	Copyright (C) 2013	Patrice Mandin
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+ 
+#ifndef RDT_EVT_H
+#define RDT_EVT_H 1
+
+/*
+00:1:inc value
+01:1:store 1
+02:1:store 2,some copies
+03:1:store 2
+04:4+2:call function, 04 NN (N from 0 to 3) MM
+05:1+3:call function, 05 NN MM PP
+06:NN:exec script at inst+2, 06 NN ....
+07:NN:exec script bytecode:07 NN ....
+08:2:08 NN
+09:2:09 NN
+
+80:o1 :1:nop?
+81:o2 :1
+82:o3 :1
+83:o4 :1
+84:o5 :4
+85:o6 :2
+86:o7 :1
+87:o8 :2
+88:o9 :2
+89:o10:
+8a:o11:
+8b:o12:1
+
+f6:1
+f7:1
+f8:1,??,word used to init stuff
+f9:0 or 3
+fa:4
+fb:1 or variable
+fc:variable
+fd:script bytescodes, variable
+fe:1:nop
+ff:0:exit event: return value in eax[2] ?
+*/
+
+/*--- Defines ---*/
+
+/*--- External types ---*/
+
+/*--- Types ---*/
+
+/*--- Functions ---*/
+
+
+#endif /* RDT_EVT_H */
