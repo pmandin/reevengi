572e8bf30b5d398e7796076ebcc7c661323d897e
Patrice Mandin <patmandin@gmail.com>
2014-05-06 21:05:30 +0000

    split update and init of texture

--- src/r_opengl/render_texture.c
+++ src/r_opengl/render_texture.c
@@ -44,6 +44,7 @@
 /*--- Functions prototypes ---*/
 
 static void upload(render_texture_t *this, int num_pal);
+static void update_init(render_texture_t *this, int num_pal);
 static void update(render_texture_t *this, int num_pal);
 static void download(render_texture_t *this);
 
@@ -108,10 +109,10 @@ static void upload(render_texture_t *this, int num_pal)
 
 	gl.BindTexture(texgl->textureTarget, texgl->texture_id[i]);
 
-	this->update(this, num_pal);
+	update_init(this, num_pal);
 }
 
-static void update(render_texture_t *this, int num_pal)
+static void update_init(render_texture_t *this, int num_pal)
 {
 	render_texture_gl_t *texgl = (render_texture_gl_t *) this;
 	int i = this->paletted ? num_pal : 0;
@@ -183,6 +184,94 @@ static void update(render_texture_t *this, int num_pal)
 
 	gl.TexImage2D(texgl->textureTarget,0, internalFormat,
 		this->pitchw, this->pitchh, 0,
+		surfaceFormat, pixelType, this->pixels
+	);
+
+	if (this->bpp == 1) {
+		if (!video.has_gl_ext_paletted_texture) {
+			gl.PixelTransferi(GL_MAP_COLOR, GL_FALSE);
+		}
+	}
+}
+
+static void update(render_texture_t *this, int num_pal)
+{
+	render_texture_gl_t *texgl;
+	int i;
+	GLenum internalFormat = GL_RGBA;
+	GLenum pixelType = GL_UNSIGNED_BYTE;
+	GLenum surfaceFormat = GL_RGBA;
+	
+	texgl = (render_texture_gl_t *) this;
+	i = this->paletted ? num_pal : 0;
+
+	assert(texgl->texture_id[i] != 0xFFFFFFFFUL);
+
+	/* Create new texture object, and upload texture */
+	gl.BindTexture(texgl->textureTarget, texgl->texture_id[i]);
+
+	/* Upload new palette */
+	switch (this->bpp) {
+		case 1:
+			surfaceFormat = GL_COLOR_INDEX;
+
+#if defined(GL_EXT_paletted_texture)
+			if (video.has_gl_ext_paletted_texture && (texgl->textureTarget==GL_TEXTURE_2D)) {
+				Uint8 mapP[256*4];
+				Uint8 *pMap = mapP;
+
+				internalFormat = GL_COLOR_INDEX8_EXT;
+				for (i=0; i<256; i++) {
+					Uint32 color = this->palettes[num_pal][i];
+
+					*pMap++ = (color>>16) & 0xff;
+					*pMap++ = (color>>8) & 0xff;
+					*pMap++ = color & 0xff;
+					*pMap++ = (color>>24) & 0xff;
+				}
+				gl.ColorTableEXT(texgl->textureTarget, GL_RGBA, 256, 
+					GL_RGBA, GL_UNSIGNED_BYTE, mapP);
+			} else
+#endif
+			{
+				GLfloat mapR[256], mapG[256], mapB[256], mapA[256];
+
+				memset(mapR, 0, sizeof(mapR));
+				memset(mapG, 0, sizeof(mapG));
+				memset(mapB, 0, sizeof(mapB));
+				memset(mapA, 0, sizeof(mapA));
+				for (i=0; i<256; i++) {
+					Uint32 color = this->palettes[num_pal][i];
+
+					mapR[i] = ((color>>16) & 0xff) / 255.0f;
+					mapG[i] = ((color>>8) & 0xff) / 255.0f;
+					mapB[i] = (color & 0xff) / 255.0f;
+					mapA[i] = ((color>>24) & 0xff) / 255.0f;
+				}
+				gl.PixelTransferi(GL_MAP_COLOR, GL_TRUE);
+				gl.PixelMapfv(GL_PIXEL_MAP_I_TO_R, 256, mapR);
+				gl.PixelMapfv(GL_PIXEL_MAP_I_TO_G, 256, mapG);
+				gl.PixelMapfv(GL_PIXEL_MAP_I_TO_B, 256, mapB);
+				gl.PixelMapfv(GL_PIXEL_MAP_I_TO_A, 256, mapA);
+			}
+			break;
+		case 2:
+			if (this->format.Bmask == 31) {
+				pixelType = GL_UNSIGNED_SHORT_5_6_5;
+				surfaceFormat = GL_RGB;
+			} else {
+				pixelType = GL_UNSIGNED_SHORT_5_5_5_1;
+			}
+			break;
+		case 3:
+			surfaceFormat = GL_RGB;
+			break;
+		case 4:
+			break;
+	}
+
+	gl.TexSubImage2D(texgl->textureTarget,0,
+		0,0,this->pitchw, this->pitchh,
 		surfaceFormat, pixelType, this->pixels
 	);
 
